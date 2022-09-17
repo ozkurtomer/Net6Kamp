15b1bb94cee29e43f66c0e6cbdb870c59d8927ad
Ömer Özkurt <ozkurtomer@outlook.com>
2022-09-18 02:20:26 +0300

    Add project files.

---
+++ RentACar/Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,21 @@
+﻿using Persistence.Contexts;
+using Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Application.Services.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<BaseDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("RentACarCampConnectionString")));
+
+            services.AddScoped<IBrandRepository, BrandRepository>();
+
+            return services;
+        }
+    }
+}
